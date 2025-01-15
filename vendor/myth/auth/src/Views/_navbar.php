7f89d7d77ceef4dcd2670a4482a11112826a6484
klawcodes <codenamezeskil@gmail.com>
2025-01-15 17:53:09 +0700

    change login color

--- vendor/myth/auth/src/Views/_navbar.php
+++ vendor/myth/auth/src/Views/_navbar.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#">Admin - DonasiKita</a>
+<nav class="navbar navbar-expand-md bg-purple-600 fixed-top">
+    <h1 class="navbar-brand text-gray-100">Admin - DonasiKita</h1>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
--- vendor/myth/auth/src/Views/login.php
+++ vendor/myth/auth/src/Views/login.php
@@ -70,7 +70,7 @@
                 <?php endif; ?>
 
                 <div>
-                    <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                         <?=lang('Auth.loginAction')?>
                     </button>
                 </div>
